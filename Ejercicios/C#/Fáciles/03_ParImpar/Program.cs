e0dc46c3f20ce9f8b8545ef457e9eba2a3454c70
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-11-03 20:48:13 -0300

    Agrego ejercicio N°3: Par o Impar
    Este commit incluye la implementación del ejercicio "Par o Impar" en C#.
    El programa solicita un número para determinar si es par o impar.

---
+++ Ejercicios/C#/Fáciles/03_ParImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese 1 numero para saber si es Par o Impar:");
+            int num = int.Parse(Console.ReadLine());
+            if(num % 2 == 0)
+                Console.Write($"El numero es Par");
+            else
+                Console.Write($"El numero es Impar");
+            Console.ReadKey();
+        }
+    }
+}
