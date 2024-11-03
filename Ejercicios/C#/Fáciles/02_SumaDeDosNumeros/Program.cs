053aadb8630c6c5c809795aa6ebe32c72a068108
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-11-03 20:40:05 -0300

    Agrego ejercicio N°2: Suma de Dos Números
    Este commit incluye la implementación del ejercicio "Suma de Dos Números" en C#.
    El programa solicita dos números, los suma e imprime el resultado.

---
+++ Ejercicios/C#/Fáciles/02_SumaDeDosNumeros/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_SumaDeDosNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese 2 numeros para sumar:");
+            int num = int.Parse(Console.ReadLine());
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write($"La suma de los numeros es: {num + num1}");
+            Console.ReadKey();
+        }
+    }
+}
