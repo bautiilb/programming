2a415faec465b69ef5bf249f65b556afdf159f1c
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-11-05 16:00:31 -0300

    Agrego ejercicio N°4: Número Mayor
    Este commit incluye la implementación del ejercicio "Número Mayor" en C#.
    El programa solicita 3 números, los compara, e imprime el mayor.

---
+++ Ejercicios/C#/Fáciles/04_NumeroMayor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_NumeroMayor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[3];
+            int aux = 0;
+            Console.WriteLine("Ingrese 3 numeros para comparar");
+            for (int i = 0; i < 3; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int i in nums)
+            {
+                if (aux < i)
+                {
+                    aux = i;
+                }
+            }
+            Console.Write($"El numero mayor es: {aux}");
+            Console.ReadKey();
+        }
+    }
+}
