8659aa3e689d34c7fad57790324446463bfbe44f
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-11-05 16:18:06 -0300

    Agrego ejercicio N°5: Área de un Círculo
    Este commit incluye la implementación del ejercicio "Área de un Círculo" en C#.
    El programa solicita el radio de un círculo, para luego devolver su área.

---
+++ Ejercicios/C#/Fáciles/05_AreaDeUnCirulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_AreaDeUnCirulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el radio de un circulo");
+            int radio = int.Parse(Console.ReadLine());
+            Console.WriteLine($"El area del circulo es: {Math.Pow(radio, 2) * Math.PI}");
+            Console.ReadKey();
+        }
+    }
+}
