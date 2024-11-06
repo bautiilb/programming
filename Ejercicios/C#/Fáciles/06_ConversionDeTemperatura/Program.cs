b6cfcdbe569f735177fa1e315dad610aaecf142d
bautiilb <139379794+bautiilb@users.noreply.github.com>
2024-11-05 21:31:54 -0300

    Agrego ejercicio N°6: Conversión de Temperatura
    Este commit incluye la implementación del ejercicio "Conversión de Temperatura" en C#.
    El programa convierte un valor ingresado en grados Celsius a grados Fahrenheit utilizando la fórmula de conversión (𝐹=𝐶×9/5+32).

---
+++ Ejercicios/C#/Fáciles/06_ConversionDeTemperatura/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ConversionDeTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero de grados celcius");
+            double celcius = double.Parse(Console.ReadLine());
+            Console.Write($"En grados Fahrenheit esto seria: °{celcius * 9/5 + 32}");
+            Console.ReadKey();
+        }
+    }
+}
